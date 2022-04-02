cf00509155eed0c2436fb078b7535ea918190fd0
Runju-Raton <runju.raton@gmail.com>
2022-04-02 19:49:27 +0600

    ternary operator

---
+++ basic/ternaryOperator.js
@@ -0,0 +1,14 @@
+var a=10;
+var b=20;
+var result1=a>b?"a is greater":"b is greater";
+var result2=a?true:false;
+console.log(result1)
+console.log(result2)
+// three number comparison
+
+var a=100;
+var b=20;
+var c=10;
+
+var calMax = (a>b)?((a>c)?"a is max":"c is max"):((b>c)?"b is max":"c is max");
+console.log(calMax)
